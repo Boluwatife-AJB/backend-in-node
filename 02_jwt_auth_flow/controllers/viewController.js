a04f58a60bb3ee2732c01e1f4c9af2b37931f6cf
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2024-10-08 10:12:15 +0100

    feat: add home page

--- 02_jwt_auth_flow/controllers/viewController.js
+++ 02_jwt_auth_flow/controllers/viewController.js
@@ -1,34 +1,34 @@
-exports.getSignUpPage = (_, res) => {
+exports.getSignUpPage = (req, res) => {
   res.render("index", {
     title: "Home page",
   });
 };
 
-exports.getSignUpPage = (_, res) => {
+exports.getSignUpPage = (req, res) => {
   res.render("sign-up", {
     title: "Sign Up",
   });
 };
 
-exports.getSignInPage = (_, res) => {
+exports.getSignInPage = (req, res) => {
   res.render("sign-in", {
     title: "Sign In",
   });
 };
 
-exports.getForgotPasswordPage = (_, res) => {
+exports.getForgotPasswordPage = (req, res) => {
   res.render("forgot-password", {
     title: "Forgot Password",
   });
 };
 
-exports.getVerifyOTPPage = (_, res) => {
+exports.getVerifyOTPPage = (req, res) => {
   res.render("verify-otp", {
     title: "Verify OTP",
   });
 };
 
-exports.getResetPasswordPage = (_, res) => {
+exports.getResetPasswordPage = (req, res) => {
   res.render("reset-password", {
     title: "Reset Password",
   });
